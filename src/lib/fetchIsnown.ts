15796feb597ab59dbc4f2a1d65623895c76684cb
Tomohiko Maeyama <tomop0513.maey@gmail.com>
2025-07-29 19:00:59 +0900

    fix/rule-card, feature/add-loading

--- src/lib/fetchIsnown.ts
+++ src/lib/fetchIsnown.ts
@@ -9,18 +9,5 @@ export async function fetchIsNown(word: string) {
     },
   });
 
-  // const text = "親譲りの無鉄砲で小供の時から損ばかりしている";
-  // return new Promise<kuromoji.IpadicFeatures[]>((resolve, reject) => {
-  //   kuromoji.builder({ dicPath: "public/dict" }).build((err, tokenizer) => {
-  //     if (err) {
-  //       console.log(err);
-  //       reject(err);
-  //     } else {
-  //       const tokens = tokenizer.tokenize(word);
-  //       console.log(tokens);
-  //       resolve(tokens);
-  //     }
-  //   });
-  // });
   return res;
 }
